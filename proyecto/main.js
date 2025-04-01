7d9e7b0241d3abf88aba6cd8ff6e57e80abbcb99
Ezequiel <ezequielh.rafaela@gmail.com>
2025-03-31 23:40:25 -0300

    spinner added

--- proyecto/main.js
+++ proyecto/main.js
@@ -12,21 +12,24 @@ const container = document.getElementById("productos-container");
 })();
 
 setTimeout(() => {
-    products.forEach(product => {
-        const card = document.createElement("li");
-        card.classList.add("card");
-
-        card.innerHTML = `
-            <div class="card" style="display:flex; align-items:center;">
-                <img src=${product.image} class="card-img-top" alt=${product.title} >
-                <div class="card-body">
-                <h5 class="card-title">$ ${product.price}</h5>
-                <p class="card-text">${product.title} </p>
-                <a href="#" class="btn btn-primary">Buy</a>
+    if (products.length > 0) {
+        products.forEach(product => {
+            const card = document.createElement("li");
+            card.classList.add("card");
+    
+            card.innerHTML = `
+                <div class="card" style="display:flex; align-items:center;">
+                    <img src=${product.image} class="card-img-top" alt=${product.title} >
+                    <div class="card-body">
+                    <h5 class="card-title">$ ${product.price}</h5>
+                    <p class="card-text">${product.title} </p>
+                    <a href="#" class="btn btn-primary">Buy</a>
+                    </div>
                 </div>
-            </div>
-        `;
-        container.appendChild(card);
-        container.classList.add('cards-container');
-    });
+            `;
+            container.appendChild(card);
+            container.classList.add('cards-container');
+        });
+        document.getElementById("grow-id").remove();
+    };
 }, 1000)
