98bcf6f017b675b0acb26e5356af46b0da433f9a
Ezequiel <ezequielh.rafaela@gmail.com>
2025-03-31 22:58:53 -0300

    add changes in main branch

--- proyecto/getProducts.js
+++ proyecto/getProducts.js
@@ -3,6 +3,6 @@ export const getProductsFromFakeApi = () => {
     const products = fetch("https://fakestoreapi.com/products")
         .then(response => response.json())
         .then(products => products);
-    return products;
+    return products
 };
 
