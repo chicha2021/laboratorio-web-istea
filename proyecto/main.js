576304e8368ab6e2e7d3a72bbcbb9ef6b893aa2e
Ezequiel <ezequielh.rafaela@gmail.com>
2025-04-13 23:22:13 -0300

    added changes

--- proyecto/main.js
+++ proyecto/main.js
@@ -1,9 +1,30 @@
 import { getProductsFromFakeApi } from "./getProducts.js"
 
 let products = [];
+let productsInCart = 0;
 const container = document.getElementById("productos-container");
 
+const setElementsToCart = () => {
+    productsInCart = JSON.parse(localStorage.getItem('cart'));
+    const element = document.querySelector('#qty-cart');
+    if (productsInCart > 0 && element) {
+        element.style.display = 'block';
+        element.innerHTML = productsInCart
+    };
+};
+
+const addToCart = (e) => {
+    const element = document.querySelector('#qty-cart');
+    if (element) {
+        element.style.display = 'block';
+        element.innerHTML = productsInCart+=1
+        window.localStorage.setItem('cart', JSON.stringify(productsInCart))
+    }
+};
+
+
 (async () => {
+    setElementsToCart();
     try {
         products = await getProductsFromFakeApi();
     } catch (error) {
@@ -11,24 +32,39 @@ const container = document.getElementById("productos-container");
     };
 })();
 
+
 setTimeout(() => {
     if (products.length > 0) {
         products.forEach(product => {
             const card = document.createElement("li");
             card.classList.add("card");
-    
             card.innerHTML = `
                 <div class="card" style="display:flex; align-items:center;">
                     <img src=${product.image} class="card-img-top" alt=${product.title} >
                     <div class="card-body">
-                    <h5 class="card-title">$ ${product.price}</h5>
-                    <p class="card-text">${product.title} </p>
-                    <a href="#" class="btn btn-primary">Buy</a>
+                        <h5 class="card-title">$ ${product.price}</h5>
+                        <p class="card-text">${product.title.substring(0,40)} </p>
+                        <p class="credit-price">Hasta 6 cuotas de $${(product.price / 6).toFixed(2)} </p>
+                    </div>
+                    <div class="button-container">
+                            <span class="cart-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                            </svg>
+                            </span>
+                            <span class="cart-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="gray" class="bi bi-cart3" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                                </svg>
+                            </span>
+                        <button href="#" class="btn btn-primary button">Comprar</button>
                     </div>
                 </div>
             `;
             container.appendChild(card);
             container.classList.add('cards-container');
+            const cartButtons = card.querySelectorAll('.cart-button');
+            cartButtons[1].addEventListener('click', addToCart);
         });
         document.getElementById("grow-id").remove();
     };
